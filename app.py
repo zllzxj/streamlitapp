9ce3271c9f6382169f05f5a4f0132ab9c6218bb3
zllzxj <925426572@qq.com>
2025-03-09 10:44:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -125,7 +125,11 @@ predata = pd.DataFrame([data]) # 将预测数据转换为DataFrame
 with expand1:
     st.dataframe(predata, use_container_width=True, hide_index=True)
 
-predata = predata[columns]
+for i in columns:
+    try:
+        predata[i]
+    except:
+        st.write(i)
 
 explainer = shap.TreeExplainer(model) # 创建SHAP解释器
 shap_values = explainer.shap_values(predata) # 计算SHAP值
